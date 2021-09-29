984dd51361652ed7555ba239c817d05ab99c4167
ryan.alexandra <ryan.alexandra@vashonsd.org>
2021-09-29 11:04:19 -0700

    Cleaned up file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    String statement= "Hello World!!";
+        String statement= "Hello World!!";
         System.out.println(statement);
     }
 }
