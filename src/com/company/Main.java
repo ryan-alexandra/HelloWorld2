934a61e413342b71e21a3ece605ba5f8230c7eeb
ryan.alexandra <ryan.alexandra@vashonsd.org>
2021-09-29 11:02:19 -0700

    Cleaned up file

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String statement= "Hello World!!";
+        System.out.println(statement);
+    }
+}
